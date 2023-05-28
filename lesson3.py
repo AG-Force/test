bd519c2d51a64a09b509f2d0b9e51f8b0d789760
AG-Force <rada712807@icloud.com>
2023-05-28 21:53:42 +0200

    first commit

---
+++ lesson3.py
@@ -0,0 +1,8 @@
+myvariable = "my value"
+print(myvariable)
+##################### the name of variables starts with _ and A to Z / a to z
+my23233 = "test"
+print(my23233)
+#####################
+my_name = 12
+print(my_name)
